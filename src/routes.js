fa5a9bf7cd3f1b43083439f71f372be6b8231461
RafaelPires2 <rafaelbrpires@gmail.com>
2022-12-30 07:37:38 -0300

    feat: routes and rules crud

--- src/routes.js
+++ src/routes.js
@@ -3,4 +3,10 @@ const router = express.Router();
 
 const CarroController = require('./controllers/CarroController');
 
+router.get('/carros', CarroController.buscarTodos);
+router.get('/carro/:codigo', CarroController.buscarUm);
+router.post('/carro', CarroController.inserir);
+router.put('/carro/:codigo', CarroController.alterar);
+router.delete('/carro/:codigo', CarroController.excluir);
+
 module.exports = router;
